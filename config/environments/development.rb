94ecf8f9276e435c3027e6ea77cbb94d047c654a
EC2 Default User <[ran.0502.orchid@gmail.com]>
2022-01-24 16:33:10 +0000

    [add]3-12

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,6 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  config.hosts << "b037fd38e1b442c182430bb4354a6b7f.vfs.cloud9.us-east-1.amazonaws.com"
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  root to: 'homes#top' #ルートパスを使用する
+  get 'books' => 'books#index'
+  get 'books' => 'books#new'
+  get 'books/:id' => 'books#show', as: 'book'
+  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
