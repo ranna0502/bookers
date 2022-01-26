fc7ec926a5bfe170806c15591034284a1fa7d508
EC2 Default User <[ran.0502.orchid@gmail.com]>
2022-01-26 06:13:36 +0000

    [finish]3-12

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,7 +8,7 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     if @book.save
       flash[:notice] = "Book was successfully created."
-      redirect_to book_path(book.id) #投稿したIDに紐づく詳細画面へ遷移
+      redirect_to book_path(@book.id) #投稿したIDに紐づく詳細画面へ遷移
     else
       @books = Book.all
       render :index
@@ -27,7 +27,7 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
     if @book.update(book_params)
       flash[:notice] = "Book was successfully updated."
-      redirect_to book_path(book.id)
+      redirect_to book_path(@book.id)
     else
       render :edit
     end
